96adc93649dffb2a0f1ef8958c7be93ce3ca81c9
Bernardo Aguiar <baguiar512@gmail.com>
2025-09-16 01:42:57 +0100

    Interface Concluída

--- TP1/src/gui/TP1_GUI.java
+++ TP1/src/gui/TP1_GUI.java
@@ -15,10 +15,11 @@ import java.awt.Color;
 import javax.swing.JSpinner;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 public class TP1_GUI {
 
-	private JFrame frame;
+	private JFrame frmAd;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -29,7 +30,7 @@ public class TP1_GUI {
 			public void run() {
 				try {
 					TP1_GUI window = new TP1_GUI();
-					window.frame.setVisible(true);
+					window.frmAd.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -42,67 +43,69 @@ public class TP1_GUI {
 	}
 
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmAd = new JFrame();
+		frmAd.setOpacity(2.0f);
+		frmAd.setTitle("GUI Trabalho Prático 1");
+		frmAd.setBounds(100, 100, 684, 545);
+		frmAd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAd.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Raio");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(49, 10, 42, 29);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(10, 10, 42, 29);
+		frmAd.getContentPane().add(lblNewLabel);
 		
 		textField = new JTextField();
 		textField.setHorizontalAlignment(SwingConstants.CENTER);
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		textField.setText("20");
-		textField.setBounds(94, 10, 48, 29);
-		frame.getContentPane().add(textField);
+		textField.setBounds(55, 10, 48, 29);
+		frmAd.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Ângulo");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(191, 10, 59, 29);
-		frame.getContentPane().add(lblNewLabel_1);
+		lblNewLabel_1.setBounds(129, 10, 59, 29);
+		frmAd.getContentPane().add(lblNewLabel_1);
 		
 		textField_1 = new JTextField();
 		textField_1.setHorizontalAlignment(SwingConstants.CENTER);
 		textField_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
 		textField_1.setText("90");
-		textField_1.setBounds(252, 11, 46, 28);
-		frame.getContentPane().add(textField_1);
+		textField_1.setBounds(190, 11, 46, 28);
+		frmAd.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Distância");
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1_1.setBounds(356, 10, 83, 29);
-		frame.getContentPane().add(lblNewLabel_1_1);
+		lblNewLabel_1_1.setBounds(263, 10, 83, 29);
+		frmAd.getContentPane().add(lblNewLabel_1_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setText("33");
 		textField_2.setHorizontalAlignment(SwingConstants.CENTER);
 		textField_2.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
 		textField_2.setColumns(10);
-		textField_2.setBounds(441, 11, 46, 28);
-		frame.getContentPane().add(textField_2);
+		textField_2.setBounds(348, 11, 46, 28);
+		frmAd.getContentPane().add(textField_2);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("Robot");
 		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1_2.setBounds(625, 10, 59, 29);
-		frame.getContentPane().add(lblNewLabel_1_2);
+		lblNewLabel_1_2.setBounds(506, 10, 59, 29);
+		frmAd.getContentPane().add(lblNewLabel_1_2);
 		
 		txtZe = new JTextField();
 		txtZe.setText("Ze");
 		txtZe.setHorizontalAlignment(SwingConstants.CENTER);
 		txtZe.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
 		txtZe.setColumns(10);
-		txtZe.setBounds(686, 11, 90, 28);
-		frame.getContentPane().add(txtZe);
+		txtZe.setBounds(567, 11, 90, 28);
+		frmAd.getContentPane().add(txtZe);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Ligar");
 		chckbxNewCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		chckbxNewCheckBox.setBounds(684, 55, 92, 28);
-		frame.getContentPane().add(chckbxNewCheckBox);
+		chckbxNewCheckBox.setBounds(567, 46, 71, 28);
+		frmAd.getContentPane().add(chckbxNewCheckBox);
 		
 		JButton btnNewButton = new JButton("FRENTE");
 		btnNewButton.setForeground(new Color(0, 0, 0));
@@ -112,8 +115,8 @@ public class TP1_GUI {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(320, 128, 133, 36);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton.setBounds(259, 106, 133, 36);
+		frmAd.getContentPane().add(btnNewButton);
 		
 		JButton btnParar = new JButton("PARAR");
 		btnParar.setBackground(new Color(217, 0, 5));
@@ -122,8 +125,8 @@ public class TP1_GUI {
 			}
 		});
 		btnParar.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnParar.setBounds(320, 162, 133, 36);
-		frame.getContentPane().add(btnParar);
+		btnParar.setBounds(259, 140, 133, 36);
+		frmAd.getContentPane().add(btnParar);
 		
 		JButton btnEsquerda = new JButton("ESQUERDA");
 		btnEsquerda.setBackground(new Color(255, 255, 0));
@@ -132,14 +135,14 @@ public class TP1_GUI {
 			}
 		});
 		btnEsquerda.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnEsquerda.setBounds(191, 162, 133, 36);
-		frame.getContentPane().add(btnEsquerda);
+		btnEsquerda.setBounds(129, 140, 133, 36);
+		frmAd.getContentPane().add(btnEsquerda);
 		
 		JButton btnDireita = new JButton("DIREITA");
 		btnDireita.setBackground(new Color(0, 128, 192));
 		btnDireita.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnDireita.setBounds(452, 162, 133, 36);
-		frame.getContentPane().add(btnDireita);
+		btnDireita.setBounds(391, 140, 133, 36);
+		frmAd.getContentPane().add(btnDireita);
 		
 		JButton btnTrs = new JButton("TRÁS");
 		btnTrs.setBackground(new Color(192, 192, 192));
@@ -148,22 +151,34 @@ public class TP1_GUI {
 			}
 		});
 		btnTrs.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnTrs.setBounds(320, 196, 133, 36);
-		frame.getContentPane().add(btnTrs);
+		btnTrs.setBounds(259, 174, 133, 36);
+		frmAd.getContentPane().add(btnTrs);
 		
 		JSpinner spinner = new JSpinner();
 		spinner.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		spinner.setBounds(383, 269, 42, 29);
-		frame.getContentPane().add(spinner);
+		spinner.setBounds(358, 221, 42, 29);
+		frmAd.getContentPane().add(spinner);
 		
 		JLabel lblNewLabel_1_3 = new JLabel("Número");
 		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_1_3.setBounds(309, 269, 71, 29);
-		frame.getContentPane().add(lblNewLabel_1_3);
+		lblNewLabel_1_3.setBounds(284, 221, 71, 29);
+		frmAd.getContentPane().add(lblNewLabel_1_3);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Movimentos Aleatórios");
 		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		rdbtnNewRadioButton.setBounds(447, 273, 216, 20);
-		frame.getContentPane().add(rdbtnNewRadioButton);
+		rdbtnNewRadioButton.setBounds(422, 225, 216, 20);
+		frmAd.getContentPane().add(rdbtnNewRadioButton);
+		
+		JLabel lblConsola = new JLabel("Consola");
+		lblConsola.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblConsola.setBounds(10, 297, 77, 29);
+		frmAd.getContentPane().add(lblConsola);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 325, 668, 181);
+		frmAd.getContentPane().add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
 	}
 }
